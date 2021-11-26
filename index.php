56acf24d988ba56f2abe131294abc57d09a0b2bb
Ardhan Ardiansyah <82355904+ardhanardiansyahh@users.noreply.github.com>
2021-11-26 15:14:28 +0700

    all update

--- index.php
+++ index.php
@@ -1,145 +1,162 @@
-<?php
-
-include "database.php";
-    if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
-if (!isset($_SESSION["email"])) {
-    echo "<script> alert('You have to login first');</script>";
-    echo "<script> location= 'login.php'; </script>";
-	exit;
-}
-
-$email=$_SESSION["email"];
+<?php include 'header.php'; ?>
 
 
-?>
+<!-- BREADCRUMB -->
+<div id="breadcrumb">
+	<div class="container">
+		<ul class="breadcrumb">
+			<li><a href="index.php">Home</a></li>
+		</ul>
+	</div>
+</div>
+<!-- /BREADCRUMB -->
 
-<!DOCTYPE html>
-<html lang="en">
+<!-- section -->
+<div class="section">
+	<!-- container -->
+	<div class="container">
+		<!-- row -->
+		<div class="row">
+			
+			<?php 
+			// include 'sidebar.php'; 
+			?>
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+			<!-- MAIN -->
+			<div id="main" class="col-md-12">
 
-  <title>TimeZone Online</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+				<!-- store top filter -->
+				<form action="" method="get">
+					<div class="store-filter clearfix">
+						<div class="pull-right">
+							<div class="sort-filter">
+								<span class="text-uppercase">Urutkan :</span>
+								<select class="input" name="urutan" onchange="this.form.submit()">
+									<option <?php if(isset($_GET['urutan']) && $_GET['urutan'] == "terbaru"){echo "selected='selected'";} ?> value="terbaru">Terbaru</option>
+									<option <?php if(isset($_GET['urutan']) && $_GET['urutan'] == "harga"){echo "selected='selected'";} ?> value="harga">Harga</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</form>
+				<!-- /store top filter -->
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+				<!-- STORE -->
+				<div id="store">
+					<!-- row -->
+					<div class="row">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
+						<?php
+						$halaman = 12;
+						$page = isset($_GET["halaman"]) ? (int)$_GET["halaman"] : 1;
+						$mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
+						$result = mysqli_query($koneksi, "SELECT * FROM produk");
+						$total = mysqli_num_rows($result);
+						$pages = ceil($total/$halaman);  
+						if(isset($_GET['urutan']) && $_GET['urutan'] == "harga"){
+							if(isset($_GET['cari'])){
+								$cari = $_GET['cari'];
+								$data = mysqli_query($koneksi,"select * from produk,kategori where kategori_id=produk_kategori and produk_nama like '%$cari%' order by produk_harga asc LIMIT $mulai, $halaman");
+							}else{
+								$data = mysqli_query($koneksi,"select * from produk,kategori where kategori_id=produk_kategori order by produk_harga asc LIMIT $mulai, $halaman");
+							}
+						}else{
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+							if(isset($_GET['cari'])){
+								$cari = $_GET['cari'];
+								$data = mysqli_query($koneksi,"select * from produk,kategori where kategori_id=produk_kategori and produk_nama like '%$cari%' order by produk_id desc LIMIT $mulai, $halaman");
+							}else{
+								$data = mysqli_query($koneksi,"select * from produk,kategori where kategori_id=produk_kategori order by produk_id desc LIMIT $mulai, $halaman");
+							}
 
-  <link href="assets/css/style.css" rel="stylesheet">
+						}          
+						$no =$mulai+1;
 
-  <div class="ocean">
-    <div class="wave"></div>
-    <div class="wave"></div>
-  </div>
+						while($d = mysqli_fetch_array($data)){
+							?>
 
-</head>
+							<div class="col-md-3 col-sm-6 col-xs-6">
+								<div class="product product-single">
+									<div class="product-thumb">
+										<div class="product-label">
+											<span><?php echo $d['kategori_nama'] ?></span>
+										</div>
 
-<body>
+										<a href="produk_detail.php?id=<?php echo $d['produk_id'] ?>" class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</a>
+										
+										<?php if($d['produk_foto1'] == ""){ ?>
+											<img src="gambar/sistem/produk.png" style="height: 250px">
+										<?php }else{ ?>
+											<img src="gambar/produk/<?php echo $d['produk_foto1'] ?>" style="height: 250px">
+										<?php } ?>
+									</div>
+									<div class="product-body">
+										<h3 class="product-price"><?php echo "Rp. ".number_format($d['produk_harga']).",-"; ?> <?php if($d['produk_jumlah'] == 0){?> <del class="product-old-price">Kosong</del> <?php } ?></h3>
+										<div class="product-rating">
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star-o empty"></i>
+										</div>
+										<h2 class="product-name"><a href="produk_detail.php?id=<?php echo $d['produk_id'] ?>"><?php echo $d['produk_nama']; ?></a></h2>
+										<div class="product-btns">
+											<a class="main-btn btn-block text-center" href="produk_detail.php?id=<?php echo $d['produk_id'] ?>"><i class="fa fa-search"></i> Lihat</a>
+											<a class="primary-btn add-to-cart btn-block text-center" href="keranjang_masukkan.php?id=<?php echo $d['produk_id']; ?>&redirect=index"><i class="fa fa-shopping-cart"></i> Masukkan Keranjang</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- /Product Single -->
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
-    
-    <div class="container d-flex align-items-center">
+							<?php 
+						}
+						?>
 
-    <h1 class="ml1">
-      <span class="text-wrapper">
-        <span class="line line1"></span>
-        <span class="letters">TimeZone</span>
-        <span class="line line2"></span>
-      </span>
-    </h1>
+					</div>
+					<!-- /row -->
+				</div>
+				<!-- /STORE -->
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+				
+				<div class="store-filter clearfix">
+					<div class="pull-right">
+						<ul class="store-pages">
+							<li><span class="text-uppercase">Page:</span></li>
+							<?php for ($i=1; $i<=$pages ; $i++){ ?>
+								<?php if($page==$i){ ?>
+									<li class="active"><?php echo $i; ?></li>
+								<?php }else{ ?>
 
+									<?php 
+									if(isset($_GET['cari'])){
+										$cari = $_GET['cari'];
+										$c = "&cari=".$cari;
+									}
+									if(isset($_GET['urutan']) && $_GET['urutan'] == "harga"){
+										?>
+										<li><a href="?halaman=<?php echo $i; ?>&urutan=harga<?php echo $c ?>"><?php echo $i; ?></a></li>
+										<?php 
+									}else{
+										?>
+										<li><a href="?halaman=<?php echo $i; ?><?php echo $c ?>"><?php echo $i; ?></a></li>
+										<?php
+									}
+									?>
 
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="./register.php">Registration</a></li>
-          <li><a class="nav-link scrollto" href="./login.php">Login</a></li>
-		  <li><a class="nav-link scrollto" href="./forums/index.php">Forums</a></li>
-        </ul>
-      </nav>
-    </div>
+								<?php } ?>
+							<?php } ?>
+						</ul>
+					</div>
+				</div>
 
-    
-  </header>
+			</div>
+			<!-- /MAIN -->
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /container -->
+</div>
+<!-- /section -->
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="clearfix">
-    <div class="container d-flex h-100">
-      <div class="row justify-content-center align-self-center" data-aos="fade-up">
-        <div class="col-lg-6 intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100">
-          <svg viewbox="0 0 100 20">
-            <defs>
-              <linearGradient id="gradient">
-                <stop color="#000"/>
-              </linearGradient>
-              <pattern id="wave" x="0" y="-0.5" width="100%" height="100%" patternUnits="userSpaceOnUse">
-                <path id="wavePath" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" mask="url(#mask)" fill="url(#gradient)"> 
-                  <animateTransform
-                      attributeName="transform"
-                      begin="0s"
-                      dur="1.6s"
-                      type="translate"
-                      from="0,0"
-                      to="30,0"
-                      repeatCount="indefinite" />
-                </path>
-              </pattern>
-            </defs>
-            <text text-anchor="middle" x="40" y="15" font-size="16" fill="blue" fill-opacity="0.4">TimeZone</text>
-            <text text-anchor="middle" x="40" y="15" font-size="16" fill="url(#wave)"  fill-opacity="1">TimeZone</text>
-          </svg>
-        </div>
-
-        <div class="col-lg-6 intro-img order-lg-last order-first" data-aos="zoom-out" data-aos-delay="200">
-          <img src="assets/img/timezone.png" alt="" class="img-fluid">
-        </div>
-      </div>
-
-    </div>
-  </section><!-- End Hero -->
-
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="section-bg">
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong>TimeZone</strong>. All Rights Reserved
-      </div>
-    </div>
-  </footer><!-- End  Footer -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/purecounter/purecounter.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+<?php include 'footer.php'; ?>
--- login.php
+++ login.php
@@ -1,72 +1,71 @@
-<?php 
-include_once 'database.php';
-
-if (isset($_SESSION['email'])) {
-    echo "<script> alert('You are already logged in'); </script>";
-    echo "<script> location='tabeltimezone.php';</script></a>";
-}
-
-?>
-
 <!DOCTYPE html>
-<html lang="en" >
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>TimeZone Online</title>
-  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css'><link rel="stylesheet" href="css/style.css">
-
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>TimeZone Online | GameShop.id</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="assets/plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body>
-    <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto active" href="/index.php">Home</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav>
-<div class="form-body">
-        <div class="row">
-            <div class="form-holder">
-                <div class="form-content">
-                    <div class="form-items">
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
-                            </div>
-                        <h3>Login Form</h3>
-                        <form action="database.php?proses_login" class="dilarikan-javascript" method="POST" novalidate>
+<body class=" bg-primary">
+  <div class="container">
+    <div class="login-box">
 
-                            <div class="col-md-12">
-                                <input class="form-control" type="email" name="email" placeholder="E-mail Address" required>
-                                 <div class="valid-feedback">Email Correct</div>
-                                 <div class="invalid-feedback">Email can't be left blank</div>
-                            </div>
+      <center>
 
 
-                           <div class="col-md-12">
-                              <input class="form-control" type="password" name="password" placeholder="Password" required>
-                               <div class="valid-feedback">Password Correct</div>
-                               <div class="invalid-feedback">Password can't be left blank</div>
-                           </div>
+        <h2>TOKO ONLINE</h2>
+        <h3>TimeZone Online | GameShop.id</h3>
 
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                          <label class="form-check-label">I agree that the email and password are correct</label>
-                         <div class="invalid-feedback">Make sure your data is correct</div>
-                        </div>
-                  
+        <br/>
 
-                            <div class="form-button mt-3">
-                                <button id="login" type="submit" class="btn btn-primary">Login</button>
-                                <a class="btn btn-primary" href="/register.php  ">Register Here</a>
-                                
-                            </div>
-                        </form>
-                    </div>
-                </div>
+        <?php 
+        if(isset($_GET['alert'])){
+          if($_GET['alert'] == "gagal"){
+            echo "<div class='alert alert-danger'>Login gagal! username dan password salah!</div>";
+          }else if($_GET['alert'] == "logout"){
+            echo "<div class='alert alert-success'>Anda telah berhasil logout</div>";
+          }else if($_GET['alert'] == "belum_login"){
+            echo "<div class='alert alert-warning'>Anda harus login untuk mengakses halaman admin</div>";
+          }
+        }
+        ?>
+      </center>
+
+      <div class="login-box-body">
+
+        <br>
+        <p class="login-box-msg text-bold">LOGIN</p>
+
+        <form action="periksa_login.php" method="POST">
+          <div class="form-group has-feedback">
+            <input type="text" class="form-control" placeholder="Username" name="username" required="required" autocomplete="off">
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="Password" name="password" required="required" autocomplete="off">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <div class="col-xs-offset-8 col-xs-4">
+              <button type="submit" class="btn btn-primary btn-block btn-flat">LOGIN</button>
             </div>
-        </div>
+          </div>
+        </form>
+
+        <br>
+      </div>
     </div>
-  <script  src="js/script.js"></script>
+  </div>
+
+
+  <script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
 </body>
 </html>
-
